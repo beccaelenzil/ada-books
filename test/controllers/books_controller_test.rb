a337a921abd1247dbae48dc568796099a0a9222a
Becca <becca@adadevelopersacademy.org>
2020-10-26 13:42:49 -0700

    show action

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -11,4 +11,20 @@ describe BooksController do
     get "/books"
     must_respond_with :success
   end
+
+  it "will get a book for valid id" do
+    valid_id = 1
+
+    get "/books/#{valid_id}"
+
+    must_respond_with :success
+  end
+
+  it "will respond with not found for invalid id" do
+    invalid_id = 999
+
+    get "/books/#{invalid_id}"
+
+    must_respond_with :not_found
+  end
 end
